1a5f6b11a26f174161d0ee3f6159971ff033a88b
Marco Moroni <marcomoroni394@gmail.com>
2018-03-19 18:53:03 +0000

    Top-down player movement

--- azucena/components/cmp_physics.cpp
+++ azucena/components/cmp_physics.cpp
@@ -32,13 +32,11 @@ PhysicsComponent::PhysicsComponent(Entity* p, bool dyn,
     b2FixtureDef FixtureDef;
     // Fixture properties
     // FixtureDef.density = _dynamic ? 10.f : 0.f;
-    FixtureDef.friction = _dynamic ? 0.1f : 0.8f;
-    FixtureDef.restitution = .2;
+	FixtureDef.friction = _dynamic ? 0.1f : 0.8f;
+	FixtureDef.restitution = /*.2;*/ 0;
     FixtureDef.shape = &Shape;
     // Add to body
     _fixture = _body->CreateFixture(&FixtureDef);
-    //_fixture->SetRestitution(.9)
-    FixtureDef.restitution = .2;
   }
 
   // An ideal Pod/capusle shape should be used for hte player,
