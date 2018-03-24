6350f487031b320d9e75cef6a3eeaaee2caedf54
Marco Moroni <marcomoroni394@gmail.com>
2018-03-24 11:44:44 +0000

    Close #14

--- azucena/main.cpp
+++ azucena/main.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include "engine.h"
 #include "game.h"
 #include "scenes/scene_menu.h"
@@ -10,5 +11,12 @@ Level2Scene level2;
 Level3Scene level3;
 
 int main() {
+
+#ifdef NDEBUG
+	// Hide console
+	HWND hwnd = GetConsoleWindow();
+	ShowWindow(hwnd, SW_HIDE);
+#endif
+
   Engine::Start(1280, 720, "Azucena", &menu);
 }
