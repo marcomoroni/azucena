9f5e12cc3f4882f3b62056adafbedebb9efa0f40
Marco Moroni <marcomoroni394@gmail.com>
2018-03-27 18:45:53 +0100

    Better game UI update function

--- azucena/components/cmp_game_ui.cpp
+++ azucena/components/cmp_game_ui.cpp
@@ -10,19 +10,26 @@ RectangleShape r;
 GameUIComponent::GameUIComponent(Entity* p)
 	: Component(p)
 {
+	// Draw a red square at top right corner
+	// (set origin to correct position)
 	r = RectangleShape({ 100.0f, 100.0f });
 	r.setFillColor(Color::Red);
+	r.setOrigin(100.0f, 0);
 }
 
 void GameUIComponent::update(double dt)
 {
 	// Move elements so that they looked fixed on the screen.
 	// This is not the best way, but we would have to change the
-	// system rendere otherwise.
+	// system renderer otherwise.
+
+	_parent->setPosition({
+		Engine::GetWindow().getView().getCenter().x,
+		Engine::GetWindow().getView().getCenter().y});
 
 	r.setPosition(
-		Engine::GetWindow().getView().getCenter().x - Engine::GetWindow().getSize().x / 2 + 10.0f,
-		Engine::GetWindow().getView().getCenter().y - Engine::GetWindow().getSize().y / 2 + 10.0f);
+		_parent->getPosition().x + Engine::GetWindow().getSize().x / 2 - 10.0f,
+		_parent->getPosition().y - Engine::GetWindow().getSize().y / 2 + 10.0f);
 }
 
 void GameUIComponent::render()
