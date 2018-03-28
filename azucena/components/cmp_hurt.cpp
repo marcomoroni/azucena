daf1504e89884bda23b834c2e396824f0986b0fb
Marco Moroni <marcomoroni394@gmail.com>
2018-03-28 18:56:48 +0100

    Write hurt component

--- azucena/components/cmp_hurt.cpp
+++ azucena/components/cmp_hurt.cpp
@@ -1,14 +1,39 @@
 #include "cmp_hurt.h"
+#include "system_physics.h"
+#include "engine.h"
+#include "cmp_physics.h"
+#include "cmp_player_health.h"
+#include "cmp_enemy_health.h"
 
-HurtComponent::HurtComponent(Entity* p)
-	: Component(p)
+using namespace std;
+using namespace sf;
+
+HurtComponent::HurtComponent(Entity* p, string tagToHurt)
+	: Component(p), _tagToHurt(tagToHurt), _active(false)
 {
 
 }
 
 void HurtComponent::update(double dt)
 {
-
+	if (_active)
+	{
+		auto cs = _parent->get_components<PhysicsComponent>()[0]->getTouching();
+		for (auto c : cs)
+		{
+			auto entsToHurt = Engine::GetActiveScene()->ents.find(_tagToHurt);
+			for (auto entityToHurt : entsToHurt)
+			{
+				if (c->GetFixtureA() == entityToHurt->get_components<PhysicsComponent>()[0]->getFixture())
+				{
+					if (_tagToHurt == "player") entityToHurt->get_components<PlayerHealthComponent>()[0]->decreaseHealth(_damage);
+					else if (_tagToHurt == "enemy") entityToHurt->get_components<EnemyHealthComponent>()[0]->decreaseHealth(_damage);
+				}
+			}
+		}
+	}
 }
 
 void HurtComponent::setDamage(int d) { _damage = d; }
+
+void HurtComponent::setActive(bool a) { _active = a; }
--- azucena/components/cmp_hurt.h
+++ azucena/components/cmp_hurt.h
@@ -1,16 +1,20 @@
 #pragma once
 
 #include <ecm.h>
+#include <string>
 
 class HurtComponent : public Component
 {
 private:
+	bool _active;
 	int _damage = 1;
+	std::string _tagToHurt;
 
 public:
 	void update(double) override;
 	void render() override {};
-	explicit HurtComponent(Entity* p);
+	explicit HurtComponent(Entity* p, std::string tagToHurt);
 	HurtComponent() = delete;
 	void setDamage(int);
+	void setActive(bool);
 };
