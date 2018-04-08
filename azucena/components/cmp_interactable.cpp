22039aa665dbeb390da321c1f959c678480c2d7f
Marco Moroni <marcomoroni394@gmail.com>
2018-04-08 18:50:16 +0200

    Pickup timer has corresponding sprites

--- azucena/components/cmp_interactable.cpp
+++ azucena/components/cmp_interactable.cpp
@@ -2,6 +2,7 @@
 #include "engine.h"
 #include "cmp_sprite.h"
 #include "system_resources.h"
+#include <SFML/Window/Keyboard.hpp>
 
 using namespace std;
 using namespace sf;
@@ -17,23 +18,61 @@ void InteractableComponent::update(double dt)
 
   if (playerIsClose)
   {
-    _pickUpTimerSprite->setVisible(true);
-    _pickUpTimerSprite->setPosition(_player->getPosition() + Vector2f(0.0f, -32.0f));
+    _interactionTimerSprite->setVisible(true);
+    _interactionTimerSprite->setPosition(_player->getPosition() + Vector2f(0.0f, 64.0f));
+    if (Keyboard::isKeyPressed(Keyboard::E))
+    {
+      _interactionTimer += dt;
+    }
+    else
+    {
+      _interactionTimer = 0.0f;
+    }
+
+    // Change sprite according to timer
+    // Note: there are 4 sprites
+    auto s = _interactionTimerSprite->get_components<SpriteComponent>()[0];
+    if (_interactionTimer == 0.0f)
+    {
+      // Empty
+      s->getSprite().setTextureRect(sf::IntRect(32 * 7, 32 * 3, 32, 32));
+    }
+    else if (_interactionTimer <= _interactionTimeNeeded / 4)
+    {
+      // 1/4 circle
+      s->getSprite().setTextureRect(sf::IntRect(32 * 8, 32 * 3, 32, 32));
+    }
+    else if (_interactionTimer <= _interactionTimeNeeded / 2)
+    {
+      // half circle
+      s->getSprite().setTextureRect(sf::IntRect(32 * 9, 32 * 3, 32, 32));
+    }
+    else if (_interactionTimer <= _interactionTimeNeeded / 4 * 3)
+    {
+      // 3/4 circle
+      s->getSprite().setTextureRect(sf::IntRect(32 * 7, 32 * 4, 32, 32));
+    }
+    else if (_interactionTimer <= _interactionTimeNeeded - 0.2f)
+    {
+      // Full circle
+      s->getSprite().setTextureRect(sf::IntRect(32 * 8, 32 * 4, 32, 32));
+    }
   }
   else
   {
-    _pickUpTimerSprite->setVisible(false);
+    _interactionTimerSprite->setVisible(false);
+    _interactionTimer = 0.0f;
   }
 }
 
-InteractableComponent::InteractableComponent(Entity* p, float playerInteractionDistance)
-  : Component(p), _playerInteractionDistance(playerInteractionDistance)
+InteractableComponent::InteractableComponent(Entity* p, float playerInteractionDistance, float interactionTime)
+  : Component(p), _playerInteractionDistance(playerInteractionDistance), _interactionTimeNeeded(interactionTime), _interactionTimer(0.0f)
 {
   _player = &(*(Engine::GetActiveScene()->ents.find("player")[0]));
 
-  _pickUpTimerSprite = &(*(Engine::GetActiveScene()->makeEntity()));
+  _interactionTimerSprite = &(*(Engine::GetActiveScene()->makeEntity()));
 
-  auto s = _pickUpTimerSprite->addComponent<SpriteComponent>();
+  auto s = _interactionTimerSprite->addComponent<SpriteComponent>();
   auto tex = Resources::get<Texture>("tex.png");
   s->setTexture(tex);
   s->getSprite().setTextureRect(sf::IntRect(32 * 7, 32 * 3, 32, 32));
--- azucena/components/cmp_interactable.h
+++ azucena/components/cmp_interactable.h
@@ -7,11 +7,13 @@ class InteractableComponent : public Component
 private:
   float _playerInteractionDistance;
   Entity* _player;
-  Entity* _pickUpTimerSprite;
+  Entity* _interactionTimerSprite;
+  float _interactionTimeNeeded;
+  float _interactionTimer;
 
 public:
   void update(double dt) override;
   void render() override {}
-  explicit InteractableComponent(Entity* p, float playerInteractionDistance = 50.0f);
+  explicit InteractableComponent(Entity* p, float playerInteractionDistance = 50.0f, float interactionTime = 1.0f);
   InteractableComponent() = delete;
 };
