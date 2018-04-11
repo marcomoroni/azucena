33ee4f4c3bae2c45218aacca04e3bf1f190ee576
Marco Moroni <marcomoroni394@gmail.com>
2018-04-11 15:37:17 +0100

    Small changes

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -40,7 +40,7 @@ void Loading_render() {
 	octagon.setRotation(loadingspinner);
 	octagon.setPosition(Vcast<float>(Engine::getWindowSize()) * .5f);
 	octagon.setFillColor(Color(255, 255, 255, min(255.f, 40.f*loadingTime)));
-	static Text t("Loading", *Resources::get<sf::Font>("RobotoMono-Regular.ttf"));
+	static Text t("Loading", *Resources::get<sf::Font>("monogram.ttf"));
 	t.setFillColor(Color(255, 255, 255, min(255.f, 40.f*loadingTime)));
 	t.setPosition(Vcast<float>(Engine::getWindowSize()) * Vector2f(0.4f, 0.3f));
 	Renderer::queue(&t);
