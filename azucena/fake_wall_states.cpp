e0dd996fda9f2808502710f8bb8bab0cdf0fe5f3
Marco Moroni <marcomoroni394@gmail.com>
2018-04-12 19:09:52 +0100

    Left level

--- azucena/fake_wall_states.cpp
+++ azucena/fake_wall_states.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void FakeWall_HiddenState::execute(Entity *owner, double dt) noexcept
 {
-	if (length(_player->getPosition() - owner->getPosition()) < 32.0f)
+	if (length(_player->getPosition() - owner->getPosition()) < 40.0f)
 	{
 		auto sm = owner->get_components<StateMachineComponent>()[0];
 		sm->changeState("revealed");
